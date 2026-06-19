6286f40f7885acb43cabac3fb9addd791eb76409
AnhTrieu04 <nguyentrieuqv@gmail.com>
2026-06-19 10:44:30 +0700

    test

---
+++ deliveryslotbookingapi/src/test/java/com/deliveryslotbookingapi/DeliveryslotbookingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliveryslotbookingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryslotbookingapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
